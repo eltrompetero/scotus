862e697392013311f3c55023ac16f11a004e4639
Eddie Lee <edl56@cornell.edu>
2017-05-15 01:02:02 -0400

    Added extra merit votes handling.

--- setup_data.py
+++ setup_data.py
@@ -60,7 +60,7 @@ def setup_canada(keepv2=True):
     print "Overwriting Canadian court information..."
     pickle.dump({'courts':courts},open('canada_full_court_votes.p','w'),-1)
 
-def setup_vinwar():
+def setup_vinwar(disp=True):
     df = pickle.load(open('vinwar_stata_EDL.xlsx.p','rb'))['df']
     names = np.sort(['mar','fort','gold','bw','stwt','whit','brn','har','mint','clk',
                      'burt','jack','doug','frk','reed','blk','war','rut','mur','vin'])
@@ -145,10 +145,117 @@ def setup_vinwar():
     confIdeVotes.replace('liberal',1,inplace=True)
     confIdeVotes.replace('',0,inplace=True)
 
+
+    # =================== 
+    # Handle merit votes.
+    # =================== 
+    # Save extra merit votes in an object array where each column corresponds to particular merits vote where
+    # an element, if not -1, contains a vector of len(names) telling us the votes.
+    # Find merits votes in columns 3-7.
+    mrtVotes = (np.zeros((len(df),5),dtype=int)-1).astype(object)
+
+    for i in xrange(3,8):
+        # Find extra merit votes.
+        extraMrtsIx = (df['votetyp%d'%i]=='MRTS').values
+        print "%d merits votes in col %d"%(extraMrtsIx.sum(),i)
+        
+        # Create an array for the set of justice votes.
+        thisVote = []
+        for n in names:
+            thisVote.append( df[n+'%dr'%i].values )
+        thisVote = np.vstack(thisVote).T
+        # Add each row of votes in separately because we are dealing with an object array.
+        for ix in np.where(extraMrtsIx)[0]:
+            mrtVotes[ix,i-3] = thisVote[ix]
+    
+    mrtVotes = shift_mrt_votes(mrtVotes)
+    if disp:
+        show_bad_mrt_votes(mrtVotes)
+    
+    # Fix these messed up votes.
+    fix_bad_mrt_votes(mrtVotes) 
+
+    # Check again.
+    if disp:
+        show_bad_mrt_votes(mrtVotes)
+    
+    # Binarize the votes.
+    bin_mrt_votes(mrtVotes)
+
+
     pickle.dump({'rptVotes':rptVotes,'rptIdeVotes':rptIdeVotes,
                  'confVotes':confVotes,'confIdeVotes':confIdeVotes,
-                 'confVoteDate':confVoteDate,'rptVoteDate':rptVoteDate},
+                 'confVoteDate':confVoteDate,'rptVoteDate':rptVoteDate,
+                 'mrtVotes':mrtVotes},
                 open('warren_conf_votes.p','wb'),-1)
+
+
+# =================================== #
+# Helper function for setup_vinwar(). #
+# =================================== #
+def bin_mrt_votes(mrtVotes):
+    for v in mrtVotes:
+        for i in v:
+            # Skip missing merit votes.
+            if type(i) is int:
+                break
+            # Replace bad vote data points.
+            for ix,j in enumerate(i):
+                if j=='grant/reverse':
+                    i[ix] = 1
+                elif j=='deny/affirm':
+                    i[ix] = -1
+
+def fix_bad_mrt_votes(mrtVotes):
+    for v in mrtVotes:
+        for i in v:
+            # Skip missing merit votes.
+            if type(i) is int:
+                break
+            # Replace bad vote data points.
+            for ix,j in enumerate(i):
+                if j==1 or j=='1.0':
+                    i[ix] = 'grant/reverse'
+                elif j==2 or j=='2.0':
+                    i[ix] = 'deny/affirm'
+                elif j=='nan':
+                    j = np.nan
+
+def show_bad_mrt_votes(mrtVotes):
+    # There seem to some cases where the vote is an integer that is not -1.
+    onlyCastVotes = np.vstack([v for v in mrtVotes.ravel() if not type(v) is int]).ravel()
+    onlyCastVotes = np.array([v for v in onlyCastVotes if check_vote(v)])
+
+    # According to vinwar_codebook.pdf, grant/reverse=1 and deny/affirm=2.
+    print "Problematic votes"
+    print np.unique(onlyCastVotes)
+    print ""
+
+def check_vote(v):
+    """Only return False if we have a value of -1."""
+    if type(v) is unicode:
+        return True
+    elif v==-1:
+        return False
+    else:
+        return True
+
+def shift_mrt_votes(mrtVotes):
+    # Push all votes as far to the left the columns as possible for organization.
+    for i,r in enumerate(mrtVotes):
+        counter = 0
+        lastZeroIx = -1
+        for counter in range(5):
+            if (not type(r[counter]) is int) and (counter>lastZeroIx):
+                r[lastZeroIx] = r[counter]
+                r[counter] = -1
+                lastZeroIx += 1
+
+    # Remove last two columns because there are no votes there anymore.
+    # array(map(lambda x:not type(x) is int,mrtVotes[:,3])).sum()
+    mrtVotes = mrtVotes[:,:3]
+    return mrtVotes
+
 
 
 if __name__=='__main__':
