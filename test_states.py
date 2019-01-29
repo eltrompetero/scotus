4fb91c982e73b7059c631f618a5cfe38c634f20f
Eddie Lee <edlee@santafe.edu>
2019-01-29 13:31:47 -0500

    added extract_nat_courts()

--- test_states.py
+++ test_states.py
@@ -6,3 +6,5 @@ from .states import *
 def test_State():
     state = State('MD')
     v = state.vote_table()
+    natCourts = state.extract_nat_courts(v)
+    assert sum([i[1] for i in natCourts])==((v>-1).values.sum(1)==len(natCourts[0][0])).sum()
