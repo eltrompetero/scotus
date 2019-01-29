725616b323b88d07a0ac5d6b215c4ab95531c5e1
Eddie Lee <el.trompetero@gmail.com>
2019-01-29 13:31:47 -0500

    added extract_nat_courts()

--- states.py
+++ states.py
@@ -32,7 +32,7 @@ class State():
         clean : bool, True
             If True, returned votes only include 0's and 1's. All other data points are set to -1.
         return_code : bool, False
-            If True, return code instead of name.
+            If True, return justice code instead of justice name.
 
         Returns
         -------
@@ -99,6 +99,50 @@ class State():
 
         voteTable[((voteTable!=0)&(voteTable!=1)).values] = -1
         return voteTable
+
+    @classmethod
+    def extract_nat_courts(cls, X, only_full_votes=True, threshold_votes='default'):
+        """Get indices for unique natural courts identified by unique subsets of voters.
+
+        Parameters
+        ----------
+        X : ndarray or pd.DataFrame
+            Formatted as such returned by cls.vote_table().
+        only_full_votes : bool, True
+            If True, only keep votes where full courts vote (indicated by the max subset size).
+        threshold_votes : int or 'default', 'default'
+            int will specify a specific number of votes
+            'default' returns courts with at least 2**n votes
+            None or False applies no threshold
+
+        Returns
+        -------
+        tuples 
+            (indices of cols that belong to nat courts, no. of votes)
+        """
+        
+        if type(X) is pd.DataFrame:
+            X = X.values
+        assert frozenset(np.unique(X))<=frozenset((-1,0,1,2,3))
+
+        natCourts = [np.where(i)[0].tolist() for i in np.unique(X>-1,axis=0)]
+        if only_full_votes:
+            mx = max([len(nc) for nc in natCourts])
+            natCourts = [i for i in natCourts if len(i)==mx]
+        else:
+            natCourts = [i for i in natCourts]
+
+        # find nat courts with longest records
+        voteCounts = np.zeros(len(natCourts))
+        for i,nc in enumerate(natCourts):
+            voteCounts[i] = (X[:,nc]>-1).all(1).sum()
+        
+        if not threshold_votes:
+            return list(zip(natCourts, voteCounts))
+        if threshold_votes=='default':
+            assert only_full_votes, 'Default for threshold_votes can only be set with only_full_votes switch.'
+            return [(i,j) for i,j in zip(natCourts, voteCounts) if j>=2**len(natCourts[0])]
+        return [(i,j) for i,j in zip(natCourts, voteCounts) if j>=threshold_votes]
 
     def _vote_cols(self):
         cols = ()
