7709618cb63d32337f27684bf535cbf11d6e6f29
Eddie Lee <el.trompetero@gmail.com>
2019-02-20 00:08:40 -0500

    added Australian High Court

--- high_courts.py
+++ high_courts.py
@@ -1,4 +1,5 @@
-# Module for loading supreme court voting data from different databases.
+# Module for loading supreme court voting data from different databases on national Supreme Courts.
+# Author : Eddie Lee, edlee@alumni.princeton.edu
 # 2016-05-16
 import pandas as pd
 import hickle, pickle
@@ -12,13 +13,26 @@ class Courts():
         return
     
     @classmethod
-    def get_court(self,name):
+    def get_court(cls,name):
         if 'can' in name.lower():
             courts = pickle.load(open('%s/%s'%(DATADR,'canada_full_court_votes.p'),'rb'))['courts']
             return courts
+        elif 'aus' in name.lower():
+            courts = pickle.load(open('%s/%s'%(DATADR,'australian_full_court_votes.p'),'rb'))['courts']
         else:
             raise Exception("Invalid court option.")
-    
+
+        return courts
+   
+    @classmethod
+    def full_court_size(cls, name):
+        if 'can' in name.lower():
+            return 9
+        elif 'aus' in name.lower():
+            return 7
+        else:
+            raise Exception("Invalid court option.")
+
     @classmethod
     def save_court(self, name, courts):
         if 'can' in name.lower():
--- setup_data.py
+++ setup_data.py
@@ -8,6 +8,118 @@ import os
 DATADR = os.path.expanduser('~')+'/Dropbox/Research/py_lib/data_sets/scotus'
 
 
+def setup_australia(min_votes=20):
+    """2019-02-19"""
+
+    df = pd.read_stata('%s/original_data_files/HCJD_Australia.dta'%DATADR, convert_categoricals=False)
+
+    # Just get the votes. These columns end in v1 and v2 (votes on issues 1 and 2).
+    colsToKeep = np.sort([c for c in df.columns if 'v_' in c])
+    df = df[colsToKeep]
+    
+
+    # Run some checks.
+    totalVotes = (np.isnan(df)==0).sum(1)>7
+    if (totalVotes>7).any():
+        print("There are votes with more than 7 voters which should be impossible.\n")
+    voteTypes = np.unique(df)
+    voteTypes = voteTypes[np.isnan(voteTypes)==0].astype(int)
+    if not set(voteTypes)<=set(range(4)):
+        print("There are vote types that are not in the specified codebook range of 0-3.")
+        print(voteTypes)
+        print()
+
+    # Find all natural courts that had full votes by looking for groups of seven voters. 
+    # We can do this by getting the list of sorted justice indices and taking the unique
+    # set of 7 voters.
+    fullVotesIx = np.where( (np.isnan(df)==0).sum(1)==7 )[0]
+    assert fullVotesIx.size>0, "There are no full votes!"
+
+    voteStrings = []
+    for i in fullVotesIx:
+        voteStrings.append( ' '.join([str(i) for i in np.where(np.isnan(df.iloc[i,:])==0)[0]]) )
+
+    voteStrings = np.unique(voteStrings)
+
+    natCourtsIx = []  # justice indices for natural courts
+    for v in voteStrings:
+        natCourtsIx.append( np.array([int(i) for i in v.split(' ')]) )
+
+    natCourtVotes = []  # votes for natural courts
+    for ix in natCourtsIx:
+        natCourtVotes.append( df.iloc[:,ix] )
+
+    fullNatCourtVotes = []  # only keeping votes where everyone is voting
+    for n in natCourtVotes:
+        fullNatCourtVotes.append(n.dropna())
+
+    # Only taking data sets with more than min votes...
+    courts = []
+    for f in fullNatCourtVotes:
+        if f.shape[0]>min_votes:
+            courts.append({})
+            courts[-1]['justices'] = [c.split('_')[1] for c in f.columns]
+            courts[-1]['votes'] = f.values
+    
+    print("Overwriting Australian court information...")
+    pickle.dump({'courts':courts}, open('%s/australian_full_court_votes.p'%DATADR,'wb'), -1)
+
+def setup_india(keepv2=True, min_votes=20):
+    """2019-02-19"""
+    df = pd.read_stata('%s/original_data_files/HCJD_India.dta'%DATADR, convert_categoricals=False)
+
+    # Just get the votes. These columns end in v1 and v2 (votes on issues 1 and 2).
+    colsToKeep = np.sort([c for c in df.columns if ('v1' in c or 'v2' in c)])
+    df = df[colsToKeep]
+    
+    colsToKeep = np.sort([c for c in df.columns if ('v1' in c)])
+    if keepv2:
+        # Add votes on the second issue as extra votes.
+        colsToAddToBottom = np.sort([c for c in df.columns if ('v2' in c)])
+        df1 = df[colsToKeep]
+        df2 = df[colsToAddToBottom]
+        df2.columns = [c.replace('2','1') for c in df2.columns]
+        
+        df = pd.concat([df1,df2])
+    else:
+        # Only keep votes on issue 1.
+        df = df[colsToKeep]
+
+
+    # Find all natural courts that had full votes by looking for groups of nine voters. 
+    # We can do this by getting the list of sorted justice indices and taking the unique
+    # set of 9 voters.
+    fullVotesIx = np.where( (np.isnan(df)==0).sum(1)==9 )[0]
+
+    voteStrings = []
+    for i in fullVotesIx:
+        voteStrings.append( ' '.join([str(i) for i in np.where(np.isnan(df.iloc[i,:])==0)[0]]) )
+
+    voteStrings = np.unique(voteStrings)
+
+    natCourtsIx = []  # justice indices for natural courts
+    for v in voteStrings:
+        natCourtsIx.append( np.array([int(i) for i in v.split(' ')]) )
+
+    natCourtVotes = []  # votes for natural courts
+    for ix in natCourtsIx:
+        natCourtVotes.append( df.iloc[:,ix] )
+
+    fullNatCourtVotes = []  # only keeping votes where everyone is voting
+    for n in natCourtVotes:
+        fullNatCourtVotes.append(n.dropna())
+
+    # Only taking data sets with more than 100 votes...
+    courts = []
+    for f in fullNatCourtVotes:
+        if f.shape[0]>min_votes:
+            courts.append({})
+            courts[-1]['justices'] = [c.split('_')[0] for c in f.columns]
+            courts[-1]['votes'] = f.values
+    
+    print("Overwriting Indian court information...")
+    pickle.dump({'courts':courts}, open('%s/india_full_court_votes.p'%DATADR,'wb'), -1)
+
 def setup_canada(keepv2=True, min_votes=20):
     """2016-05-16"""
     df = pd.read_stata('%s/original_data_files/HCJD_Canada.dta'%DATADR, convert_categoricals=False)
